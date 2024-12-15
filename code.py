4bea94257cf35689db70a68e41ffa267e692b294
WedsonWestar <102345180+WedsonWestar@users.noreply.github.com>
2024-12-15 16:07:46 +0300

    Create code.py
    First iteration of code.py file

---
+++ code.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+def main():
+    verticies = int(input())
+    graph = [[] for _ in range(verticies)]
+    distances = [-1] * verticies
+    queue = deque()
+
+    for i in range(verticies):
+        connections = int(input())
+        for j in range(connections):
+            temporary = int(input())
+            graph[i].append(temporary)
+
+    queue.append(0)
+    distances[0] = 0
+    while queue:
+        temporary = queue.popleft()
+        for neighbor in graph[temporary]:
+            if distances[neighbor] == -1:
+                queue.append(neighbor)
+                distances[neighbor] = distances[temporary] + 1
+
+    print(' '.join(map(str, distances)))
+
+if __name__ == "__main__":
+    main()
