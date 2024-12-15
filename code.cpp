c778d28c48f345282d5584e01367c5bada461199
WedsonWestar <102345180+WedsonWestar@users.noreply.github.com>
2024-12-15 16:11:28 +0300

    Create code.cpp
    First iteration of code.cpp file

---
+++ code.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <iostream>
+#include <deque>
+
+int main() {
+	int verticies = 0, connections = 0, temporary = 0;
+	std::cin >> verticies;
+	std::vector<std::vector<int> > graph(verticies, std::vector<int>(0));
+	std::vector<int> distances(verticies, -1);
+	std::deque<int> queue{};
+
+	for (int i = 0; i < verticies; i++) {
+		std::cin >> connections;
+		for (int j = 0; j < connections; j++) {
+			std::cin >> temporary;
+			graph[i].push_back(temporary);
+		}
+	}
+
+	queue.push_front(0);
+	distances[0] = 0;
+	while (!queue.empty()) {
+		temporary = queue.front();
+		queue.pop_front();
+		for (int i = 0; i < graph[temporary].size(); i++) {
+			if (distances[graph[temporary][i]] == -1) {
+				queue.push_front(graph[temporary][i]);
+				distances[graph[temporary][i]] = distances[temporary] + 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < verticies; i++) {
+		std::cout << distances[i] << ' ';
+	}
+	std::cout << std::endl;
+
+	return 0;
+}
